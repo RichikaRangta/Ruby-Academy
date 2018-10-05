44e50ffa97d8cb8fc837e50c2e4f98e1d1f01573
Chris Simpson <chris.simpson@onthebeach.co.uk>
2018-10-05 15:00:30 +0100

    Add tests for numbers 11 to 19

--- the-language/number_words/spec/number_words_spec.rb
+++ the-language/number_words/spec/number_words_spec.rb
@@ -13,7 +13,16 @@ RSpec.describe "number_words" do
     expect(number_words(9)).to eq("nine")
   end
 
-  it "returns the number 10 as a string" do
+  it "returns the numbers 10 to 19 as strings" do
     expect(number_words(10)).to eq("ten")
+    expect(number_words(10)).to eq("eleven")
+    expect(number_words(10)).to eq("twelve")
+    expect(number_words(10)).to eq("thirteen")
+    expect(number_words(10)).to eq("fourteen")
+    expect(number_words(10)).to eq("fifteen")
+    expect(number_words(10)).to eq("sixteen")
+    expect(number_words(10)).to eq("seventeen")
+    expect(number_words(10)).to eq("eighteen")
+    expect(number_words(10)).to eq("nineteen")
   end
 end
