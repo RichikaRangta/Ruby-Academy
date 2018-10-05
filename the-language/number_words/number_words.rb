ed501bb009f26e1586c1ed1f5f0e8c5fa2bff15d
Chris Simpson <chris.simpson@onthebeach.co.uk>
2018-10-05 15:04:25 +0100

    Amend method to return strings for all numbers 11 to 19

--- the-language/number_words/number_words.rb
+++ the-language/number_words/number_words.rb
@@ -1,8 +1,9 @@
 def number_words(int)
   singles = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+  doubles = { 10 => "ten", 11 => "eleven", 12 => "twelve", 13 => "thirteen", 14 => "fourteen", 15 => "fifteen", 16 => "sixteen", 17 => "seventeen", 18 => "eighteen", 19 => "nineteen" }
 
   if int.to_s.length > 1
-    "ten"
+    doubles[int]
   else
     singles[int]
   end
