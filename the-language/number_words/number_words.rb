904c1a282cdbf67a42c859e5c7210f4ba1c4c368
Chris Simpson <chris.simpson@onthebeach.co.uk>
2018-10-05 15:07:55 +0100

    Amend method to return string 20

--- the-language/number_words/number_words.rb
+++ the-language/number_words/number_words.rb
@@ -2,9 +2,13 @@ def number_words(int)
   singles = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
   doubles = { 10 => "ten", 11 => "eleven", 12 => "twelve", 13 => "thirteen", 14 => "fourteen", 15 => "fifteen", 16 => "sixteen", 17 => "seventeen", 18 => "eighteen", 19 => "nineteen" }
 
-  if int.to_s.length > 1
-    doubles[int]
+  if int == 20
+    "twenty"
   else
-    singles[int]
+    if int.to_s.length > 1
+      doubles[int]
+    else
+      singles[int]
+    end
   end
 end
