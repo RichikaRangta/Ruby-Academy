af070bc91482de2c01890d93c3f7dc1ffaf7b065
Chris Simpson <chris.simpson@onthebeach.co.uk>
2018-10-05 14:58:41 +0100

    Amend method to return string ten if number has two or more digits

--- the-language/number_words/number_words.rb
+++ the-language/number_words/number_words.rb
@@ -1,5 +1,9 @@
 def number_words(int)
   singles = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
 
-  singles[int]
+  if int.to_s.length > 1
+    "ten"
+  else
+    singles[int]
+  end
 end
