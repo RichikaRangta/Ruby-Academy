7664ac6ba0c4880fb99af3c42f2fc58eb5e28ef6
Chris Simpson <chris.simpson@onthebeach.co.uk>
2018-10-05 14:47:44 +0100

    Add tests for numbers one to nine

--- the-language/number_words/spec/number_words_spec.rb
+++ the-language/number_words/spec/number_words_spec.rb
@@ -1,7 +1,15 @@
 require_relative "../number_words"
 
 RSpec.describe "number_words" do
-  it "returns 1 as a string" do
+  it "returns the numbers 1 to 9 as strings" do
     expect(number_words(1)).to eq("one")
+    expect(number_words(2)).to eq("two")
+    expect(number_words(3)).to eq("three")
+    expect(number_words(4)).to eq("four")
+    expect(number_words(5)).to eq("five")
+    expect(number_words(6)).to eq("six")
+    expect(number_words(7)).to eq("seven")
+    expect(number_words(8)).to eq("eight")
+    expect(number_words(9)).to eq("nine")
   end
 end
