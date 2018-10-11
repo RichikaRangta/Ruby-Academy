8343dd3c2295cd31ef05eb37bee19189c29ad3d1
Chris Simpson <chris.simpson@onthebeach.co.uk>
2018-10-11 15:55:33 +0100

    Refactoring

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -8,27 +8,71 @@ class Bottles
   end
 
   def verse(number)
-    case number
-    when 0
-      <<~VERSE
-        No more bottles of beer on the wall, no more bottles of beer.
-        Go to the store and buy some more, 99 bottles of beer on the wall.
-        VERSE
-    when 1
-      <<~VERSE
-        1 bottle of beer on the wall, 1 bottle of beer.
-        Take it down and pass it around, no more bottles of beer on the wall.
-        VERSE
-    when 2
-      <<~VERSE
-        2 bottles of beer on the wall, 2 bottles of beer.
-        Take one down and pass it around, 1 bottle of beer on the wall.
-        VERSE
-    else
-      <<~VERSE
-        #{number} bottles of beer on the wall, #{number} bottles of beer.
-        Take one down and pass it around, #{number - 1} bottles of beer on the wall.
-        VERSE
+    bottle_number = BottleNumber.create(number)
+    next_bottle_number = BottleNumber.create(number - 1)
+    <<~VERSE
+      #{bottle_number.amount_left(number).capitalize} #{bottle_number.recepticle(number)} of beer on the wall, #{bottle_number.amount_left(number)} #{bottle_number.recepticle(number)} of beer.
+      #{bottle_number.action} #{next_bottle_number.recepticle(number)} of beer on the wall.
+      VERSE
+  end
+
+  class BottleNumber
+    def self.create(number)
+      if number == 0
+        BottleNumber0.new(number)
+      elsif number == 1
+        BottleNumber1.new(number)
+      else
+        BottleNumber.new(number)
+      end
+    end
+
+    def initialize(number)
+      @number = number
+    end
+
+    def recepticle(number)
+      "bottles"
+    end
+
+    def pronoun
+      if @number == 1
+        "it"
+      else
+        "one"
+      end
+    end
+
+    def amount_left(number)
+      if number == 0
+        "no more"
+      else
+        number.to_s
+      end
+    end
+
+    def action
+      "Take #{pronoun} down and pass it around, #{amount_left(predecessor)}"
+    end
+
+    def predecessor
+      @number - 1
+    end
+  end
+
+  class BottleNumber0 < BottleNumber
+    def action
+      "Go to the store and buy some more, 99"
+    end
+
+    def predecessor
+      99
+    end
+  end
+
+  class BottleNumber1 < BottleNumber
+    def recepticle(number)
+      "bottle"
     end
   end
 end
